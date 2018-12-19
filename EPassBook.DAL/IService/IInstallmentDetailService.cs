963b7a46aa2f016ec80b07661d67614b7c396600
Syedatheruddin <39413237+Syedatheruddin@users.noreply.github.com>
2018-12-19 14:46:37 +0530

    added workflow controller
    added workflow controller

---
+++ EPassBook.DAL/IService/IInstallmentDetailService.cs
@@ -0,0 +1,15 @@
+﻿using EPassBook.DAL.DBModel;
+using System.Collections.Generic;
+
+namespace EPassBook.DAL.IService
+{
+    public interface IInstallmentDetailService
+    {
+        IEnumerable<InstallmentDetail> GetAllInstallmentDetails();
+        InstallmentDetail GetInstallmentDetailById(int id);
+        void Insert(InstallmentDetail installmentDetail);
+        void Update(InstallmentDetail installmentDetail);
+        void Delete(int id);
+        void SaveChanges();        
+    }
+}
---
+++ EPassBook/Controllers/WorkFlowController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using EPassBook.DAL.DBModel;
+using EPassBook.DAL.IService;
+using EPassBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPassBook.Controllers
+{
+    public class WorkFlowController : Controller
+    {
+        private readonly IMapper _mapper;
+        IInstallmentDetailService _installmentDetailService;
+
+        public WorkFlowController(IMapper mapper, IInstallmentDetailService installmentDetailService)
+        {
+            _installmentDetailService = installmentDetailService;
+            _mapper = mapper;
+        }
+        // GET: WorkFlow
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateAccountant()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAccountant(InstallmentDetailsViewModel installmentDetailViewModel)
+        {
+            var installmet = _installmentDetailService.GetInstallmentDetailById(12);
+
+            installmet.TransactionID = installmentDetailViewModel.TransactionID;
+
+            var comment = new Comment();
+            comment.Comments = installmentDetailViewModel._Comments;
+            comment.CompanyID = 1;
+
+            installmet.Comments.Add(comment);
+
+            installmet.ModifiedBy = "Admin";
+            installmet.ModifiedDate = DateTime.Now;
+
+
+
+            _installmentDetailService.Update(installmet);
+            _installmentDetailService.SaveChanges();
+
+            return View();
+        }
+    }
+}
