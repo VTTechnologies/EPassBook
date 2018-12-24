2a3f27169f577895a541ccd7a5c6a3a3282b5f61
shakeeb <contacttoshakeeb@gmail.com>
2018-12-24 19:04:30 +0530

    Added Api Controller

---
+++ EPassBook/Controllers/APIWorkFlowController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using EPassBook.DAL.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EPassBook.Controllers
+{
+    public class APIWorkFlowController : ApiController
+    {
+        private readonly IMapper _mapper;
+        IUserService _userService;
+        ICommentService _icommentService;
+
+        public APIWorkFlowController(IUserService userService, IMapper mapper, ICommentService commentService)
+        {
+            _icommentService = commentService;
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+
+    }
+}
